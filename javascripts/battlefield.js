ba91b12a8c5f7138b29cbb0a5cdbf7e2a5912b4b
Jeremy C. Swain <jeremycswain@icloud.com>
2016-03-03 11:41:15 -0600

    MVP functionality

--- javascripts/battlefield.js
+++ javascripts/battlefield.js
@@ -1,18 +1,29 @@
     "use strict"
 function outputDOM() {
-
-    $('.intelligence').append("<p>Intelligence:" + theHero.intelligence +"</p>");
-    $('.strength').append("<p>Strength:" + theHero.strength +"</p>");
-    $('.health').append("<p>Health:" + theHero.health +"</p>");
-    $('.dexterity').append("<p>Dexterity:" + theHero.dexterity +"</p>");
-    $('.enemyInt').append("Intelligence:" + theVillian.intelligence);
-    $('.enemyStr').append("Strength:" + theVillian.strength);
-    $('.enemyHP').append("Health:" + theVillian.health);
-    $('.enemyDex').append("Dexterity:" + theVillian.dexterity);
+    $('#heroHeading').html(theHero.playerName);
+    $('#enemyHeading').html(theVillian.species + " " + theVillian.class.name);
+    $('.intelligence').html("<p>Intelligence:" + theHero.intelligence +"</p>");
+    $('.strength').html("<p>Strength:" + theHero.strength +"</p>");
+    $('.health').html("<p>Health:" + theHero.health +"</p>");
+    $('.dexterity').html("<p>Dexterity:" + theHero.dexterity +"</p>");
+    $('.enemyInt').html("Intelligence:" + theVillian.intelligence);
+    $('.enemyStr').html("Strength:" + theVillian.strength);
+    $('.enemyHP').html("Health:" + theVillian.health);
+    $('.enemyDex').html("Dexterity:" + theVillian.dexterity);
 
 };
 
 $("div#attack-button").click(function(){
-	calcDamage(theHero);
-	calcDamage(theVillian);
+    theVillian.health = (theVillian.health - calcDamage(theHero));
+    theHero.health = (theHero.health - calcDamage(theVillian));
+
+    if (theHero.health <= 0) {
+        alert("YOU ARE DEAD!")
+    } else if (theVillian.health <= 0) {
+        alert("YOU HAVE WON THE BATTLE!")
+    };
+
+    outputDOM();
 });
+
+
